2d599726338bb02f89442ebd97920d3f9b013ebf
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-26 15:12:26 +0600

    created separate route and controller files

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (_req, res) => {
+  res.send("Sign up route called");
+};
+
+export const login = async (_req, res) => {
+  res.send("Login route called");
+};
+
+export const logout = async (_req, res) => {
+  res.send("Logout route called");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// Routes
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Authentication
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
