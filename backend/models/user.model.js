5b5f2830fbec5025a66b16ab417618406567010d
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-27 09:39:25 +0600

    user model creation completed
    In this model, I created user schema, user model, hash the password and compare the password also. Finally return the model.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -40,3 +41,25 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+const User = mongoose.model("User", userSchema);
+
+// Pre save hook to hash password before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default User;
