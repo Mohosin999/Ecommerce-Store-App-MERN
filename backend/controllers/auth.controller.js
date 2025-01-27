23103da477e01a52ede6027cdbb194accd454d35
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-27 11:29:44 +0600

    trying to implement redis to cash tokens

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,47 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { redis } from "../lib/redis.js";
+
+dotenv.config();
+
+// Generate token
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+// Store token
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  );
+};
+
+// Setcookies function
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // Prevent XSS attacks
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // Prevent XSS attacks
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
 
 export const signup = async (_req, res) => {
   const { name, email, password } = _req.body;
@@ -13,6 +56,13 @@ export const signup = async (_req, res) => {
 
     // Create new user
     const newUser = await User.create({ name, email, password });
+
+    // Authenticate user
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    // Set tokens inside cookie
+    setCookies(res, accessToken, refreshToken);
 
     res.status(201).json({ newUser, message: "User created successfully" });
   } catch (error) {
