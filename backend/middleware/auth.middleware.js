5aa0486461eabce41d1b507d3f24e5b9db7b7f29
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-02-07 09:42:11 +0600

    implemented protecte and admin middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -11,18 +11,37 @@ export const protectRoute = async (req, res, next) => {
         .json({ message: "Unauthorized - No access token" });
     }
 
-    const decoded = jwt.verify(acccessToken, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decoded.userId).select("-password");
+    try {
+      const decoded = jwt.verify(acccessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId).select("-password");
 
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized - User not found" });
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - User not found" });
+      }
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - User not found" });
+      }
+      throw error;
     }
-
-    req.user = user;
-
-    next();
   } catch (error) {
     console.log("Error in protectRoute middleware", error.message);
     res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin only" });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAllProducts } from "./../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "./../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
